2bdd95f8f91da25bad4cfcc43c1664691c807d9a
Samantha Kania <samantha.kania12@gmail.com>
2019-02-12 18:07:27 +0000

    express server

---
+++ express_server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
