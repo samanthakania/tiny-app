1c3fb895590ac45e4f410ad8eeb604578f6ea3ce
Samantha Kania <samantha.kania12@gmail.com>
2019-02-12 22:21:28 +0000

    short url

--- express_server.js
+++ express_server.js
@@ -24,3 +24,16 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.send("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
